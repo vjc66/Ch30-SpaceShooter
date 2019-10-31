6eb2bc49b72d00a10dc927416b47bce7246d0d1e
vjc66 <55262819+vjc66@users.noreply.github.com>
2019-10-31 14:22:48 -0400

    enemy prefabs and enemy 0 script

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;      // The speed in m/s
+    public float fireRate = 0.3f;  // Seconds/shot (Unused)
+    public float health = 10;
+    public int score = 100;      // Points earned for destroying this
+
+    // This is a Property: A method that acts like a field
+    public Vector3 pos
+    {                                                     // a
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public virtual void Move()
+    {                                             // b
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
